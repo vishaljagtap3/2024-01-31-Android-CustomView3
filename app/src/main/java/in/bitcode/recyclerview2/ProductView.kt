d16cf8113f2a66630119d87cfd264841914468a9
vishal <vishal.jagtap@bitcode.in>
2024-03-05 15:57:17 +0530

    Compound view

---
+++ app/src/main/java/in/bitcode/recyclerview2/ProductView.kt
@@ -0,0 +1,52 @@
+package `in`.bitcode.recyclerview2
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+
+class ProductView(
+    context: Context,
+    attributeSet: AttributeSet? = null
+) : LinearLayout(context, attributeSet){
+
+    private val imgProduct : ImageView
+    private val imgDeleteIcon : ImageView
+    private val txtProductTitle : TextView
+    private val txtProductPrice : TextView
+
+    interface OnDeleteClickListener {
+        fun onDeleteClick(product: Product)
+    }
+
+    var onDeleteClickListener : OnDeleteClickListener? = null
+
+    init {
+        val view = LayoutInflater.from(context).inflate(R.layout.product_view, null)
+        addView(view)
+
+        imgProduct = view.findViewById(R.id.imgProduct)
+        imgDeleteIcon = view.findViewById(R.id.imgDeleteProduct)
+        txtProductTitle = view.findViewById(R.id.txtProductTitle)
+        txtProductPrice = view.findViewById(R.id.txtProductPrice)
+
+        imgDeleteIcon.setOnClickListener {
+            onDeleteClickListener?.onDeleteClick(product!!)
+        }
+    }
+
+    var product : Product? = null
+        set(value) {
+            field = value
+            if(value == null) {
+                return
+            }
+            imgProduct.setImageResource(value!!.imageId)
+            imgDeleteIcon.setImageResource(android.R.drawable.ic_menu_delete)
+            txtProductPrice.text = "${value.price}"
+            txtProductTitle.text = value.title
+
+        }
+}
--- app/src/main/java/in/bitcode/recyclerview2/ProductsAdapter.kt
+++ app/src/main/java/in/bitcode/recyclerview2/ProductsAdapter.kt
@@ -14,67 +14,32 @@ class ProductsAdapter(
     private val products: ArrayList<Product>
 ) : RecyclerView.Adapter<ProductsAdapter.ProductViewHolder>() {
 
-    inner class ProductViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
-        val imgProduct : ImageView
-        val txtProductTitle : TextView
-        val txtProductPrice : TextView
-        val imgDeleteProduct : ImageView
-        init {
-            imgProduct = view.findViewById(R.id.imgProduct)
-            txtProductTitle = view.findViewById(R.id.txtProductTitle)
-            txtProductPrice = view.findViewById(R.id.txtProductPrice)
-            imgDeleteProduct = view.findViewById(R.id.imgDeleteProduct)
+    inner class ProductViewHolder(val productView: ProductView) : RecyclerView.ViewHolder(productView) {
 
-            itemView.setOnClickListener {
+        init {
+            productView.setOnClickListener {
                 val product = products[adapterPosition]
                 Toast.makeText(it.context, "Product Selected: ${product.title}", Toast.LENGTH_LONG).show()
             }
 
-            imgDeleteProduct.setOnClickListener {
-                products.removeAt(adapterPosition)
-                notifyItemRemoved(adapterPosition)
-                //this@ProductsAdapter.notifyDataSetChanged()
+            productView.onDeleteClickListener = object : ProductView.OnDeleteClickListener{
+                override fun onDeleteClick(product: Product) {
+                    products.removeAt(adapterPosition)
+                    notifyItemRemoved(adapterPosition)
+                }
             }
+
         }
     }
 
     override fun getItemCount() = products.size
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.product_view, null)
-
-        //Not a good place to setup listener as we don't know what data the view would be presenting
-        /*view.setOnClickListener {
-            Toast.makeText(it.context, "Product Selected", Toast.LENGTH_LONG).show()
-        }*/
-
-        return ProductViewHolder(view)
-
-        /*return ProductViewHolder(
-            LayoutInflater.from(parent.context)
-                .inflate(
-                    R.layout.product_view, null
-                )
-        )*/
+        return ProductViewHolder( ProductView(parent.context) )
     }
 
     override fun onBindViewHolder(holder: ProductViewHolder, position: Int) {
-        val product = products[position]
-        holder.imgProduct.setImageResource(product.imageId)
-        holder.txtProductTitle.text = product.title
-        holder.txtProductPrice.text = "${product.price}"
-
-        //set up listener here
-        //this way we get the product associated with the view
-        //this is still not good! Because the old listener has to be garbage collected and new listener has to be attached to the view
-        /*holder.view.setOnClickListener { it ->
-            Toast.makeText(
-                it.context,
-                "Product ${product.title} selected...",
-                Toast.LENGTH_LONG
-            ).show()
-        }*/
+        holder.productView.product = products[position]
     }
 
 }
